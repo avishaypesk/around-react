a72af07a890587a145f03a78e7a1c2a6f417b274
Avishay <avishaypesk@gmail.com>
2022-07-22 19:54:47 +0300

    initial commit

---
+++ components/App.js
@@ -0,0 +1,162 @@
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="page">
+      <Header />
+      <Main />
+      <Footer />
+
+      <template id="card-template">
+        <article className="places__card">
+          <button
+            type="button"
+            className="button places__remove-button"
+          ></button>
+          <img src=" " alt="" className="places__image" />
+          <div className="places__bottom">
+            <h2 className="places__title"></h2>
+            <div className="places__like-section">
+              <button
+                type="button"
+                className="button places__like-button"
+              ></button>
+              <span className="places__like-count"></span>
+            </div>
+          </div>
+        </article>
+      </template>
+
+      <div className="popup form form_type_profile">
+        <div className="form__container form__container_type_profile">
+          <button type="button" className="button popup__close-button"></button>
+          <form
+            className="form form__edit-profile popup__form"
+            name="profileForm"
+            novalidate
+          >
+            <h3 className="form__title">Edit profile</h3>
+            <input
+              type="text"
+              placeholder="Name"
+              id="name-input"
+              className="form__input form__input_type_name"
+              name="profileName"
+              minlength="2"
+              maxlength="40"
+              required
+            />
+            <span id="name-input-error" className="form__input-error"></span>
+            <input
+              type="text"
+              placeholder="Title"
+              id="title-input"
+              className="form__input form__input_type_title"
+              name="profileTitle"
+              minlength="2"
+              maxlength="200"
+              required
+            />
+            <span id="title-input-error" className="form__input-error"></span>
+            <button type="submit" className="button form__save-button">
+              Save
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div className="popup form form_type_new-card">
+        <div className="form__container form__container_type_new-card">
+          <button type="button" className="button popup__close-button"></button>
+          <form
+            className="form form__new-card popup__form"
+            name="newCardForm"
+            novalidate
+          >
+            <h3 className="form__title">New place</h3>
+            <input
+              type="text"
+              placeholder="Title"
+              id="img-title-input"
+              className="form__input form__input_type_img-title"
+              name="newCardName"
+              required
+              minlength="1"
+              maxlength="30"
+            />
+            <span
+              id="img-title-input-error"
+              className="form__input-error"
+            ></span>
+            <input
+              type="url"
+              placeholder="Image link"
+              id="img-link-input"
+              className="form__input form__input_type_img-link"
+              name="newCardLink"
+              required
+            />
+            <span
+              id="img-link-input-error"
+              className="form__input-error"
+            ></span>
+            <button type="submit" className="button form__save-button">
+              Create
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div className="popup form form_type_profile-avatar">
+        <div className="form__container">
+          <button type="button" className="button popup__close-button"></button>
+          <form
+            className="form form__profile-avatar popup__form"
+            name="profileAvatarForm"
+            novalidate
+          >
+            <h3 className="form__title">Change Profile Picture</h3>
+            <input
+              type="url"
+              placeholder="Image URL"
+              id="profile-avatar-url-input"
+              className="form__input form__input_type_avatar-url"
+              name="profileImageUrlInput"
+              required
+              minlength="1"
+            />
+            <span
+              id="profile-avatar-url-input-error"
+              className="form__input-error"
+            ></span>
+            <button type="submit" className="button form__save-button">
+              Save
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div className="popup form form_type_delete-confirm">
+        <div className="form__container">
+          <button type="button" className="button popup__close-button"></button>
+          <div className="form form__delete-confirm popup__form">
+            <h3 className="form__title">Are you sure?</h3>
+            <button type="submit" className="button form__save-button"></button>
+          </div>
+        </div>
+      </div>
+
+      <div className="popup preview">
+        <div className="preview__window">
+          <button type="button" className="popup__close-button"></button>
+          <img src=" " alt="Preview Image" className="preview__preview-image" />
+          <p className="preview__description"></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
